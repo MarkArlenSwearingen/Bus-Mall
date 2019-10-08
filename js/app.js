6c98fae4c5df6de6e13e44cafbf087a5c5340ee3
Mark Swearingen <markswearingen@bendcable.com>
2019-10-08 15:06:41 -0700

    Refactored for random selection of images

--- js/app.js
+++ js/app.js
@@ -17,17 +17,17 @@ var rightImageIndex = '';
 
 // Constructor Function
 var ProductImage = function(name, pathToImg) {
-  this. name = name;
+  this.name = name;
   this.pathToImg = pathToImg;
-  // this.clicks = clicks;
-  // this.displayed = displayed;
+  this.clicks = 0;
+  this.displayed = 0;
   allProducts.push(this);
 };
 
 
 new ProductImage('bag', './img/bag.jpg');
 new ProductImage('boots', './img/boots.jpg');
-new ProductImage('chair', './img/chair.jpg');   
+new ProductImage('chair', './img/chair.jpg');
 new ProductImage('scissors', './img/scissors.jpg');
 new ProductImage('unicorn', './img/unicorn.jpg');
 new ProductImage('wine-glass', './img/wine-glass.jpg');
@@ -40,34 +40,43 @@ new ProductImage('usb', './img/usb.gif');
 new ProductImage('bathroom', './img/bathroom.jpg');
 new ProductImage('bubblegum', './img/bubblegum.jpg');
 new ProductImage('dog-duck', './img/dog-duck.jpg');
-new ProductImage('bubblegum', './img/bubblegum.jpg');
+// new ProductImage('bubblegum', './img/bubblegum.jpg');
 new ProductImage('pet-sweep', './img/pet-sweep.jpg');
 new ProductImage('tauntaun', './img/tauntaun.jpg');
 new ProductImage('water-can', './img/water-can.jpg');
 
-var renderNewImages = function(leftImageIndex, centerImageIndex, rightImageIndex){ 
-  leftImgTag.src = allProducts[leftImageIndex].pathToImg; 
-  centerImgTag.src = allProducts[centerImageIndex].pathToImg; 
-  rightImgTag.src = allProducts[rightImageIndex].pathToImg; 
+var renderNewImages = function(leftImageIndex, centerImageIndex, rightImageIndex){
+  // console.log('hello');
+  leftImgTag.src = allProducts[leftImageIndex].pathToImg;
+  centerImgTag.src = allProducts[centerImageIndex].pathToImg;
+  rightImgTag.src = allProducts[rightImageIndex].pathToImg;
 };
 
 //Event Handler
 var imageDivTag = document.getElementById('images');
 
-var handleClicks = function() {
-  var leftImageIndex = Math.floor(Math.random() * allProducts.length);
+//Randomizer
+var randomizer = function(){
+  leftImageIndex = Math.floor(Math.random() * allProducts.length);
   do {
-    var centerImageIndex = Math.floor(Math.random() * allProducts.length);
+    centerImageIndex = Math.floor(Math.random() * allProducts.length);
   } while (centerImageIndex === leftImageIndex);{
-      do {
-        var rightImageIndex = Math.floor(Math.random() * allProducts.length);
-      } while (rightImageIndex === leftImageIndex || rightImageIndex ===centerImageIndex);
-      var leftImgPath = allProducts[leftImageIndex].pathToImg; 
-      var centerImgPath = allProducts[centerImageIndex].pathToImg;
-      var rightImgPath = allProducts[rightImageIndex].pathToImg;
-    }
-    renderNewImages(leftImageIndex, centerImageIndex, rightImageIndex);
-}
+    do {
+      rightImageIndex = Math.floor(Math.random() * allProducts.length);
+    } while (rightImageIndex === leftImageIndex || rightImageIndex ===centerImageIndex);
+    leftImgPath = allProducts[leftImageIndex].pathToImg;
+    centerImgPath = allProducts[centerImageIndex].pathToImg;
+    rightImgPath = allProducts[rightImageIndex].pathToImg;
+  }
+};
+
+var handleClicks = function() {
+  randomizer();
+  totalClicks ++;
+  console.log(totalClicks);
+  allProducts[leftImageIndex].displayed ++;
+  renderNewImages(leftImageIndex, centerImageIndex, rightImageIndex);
+};
 
 imageDivTag.addEventListener('click', handleClicks);
 
